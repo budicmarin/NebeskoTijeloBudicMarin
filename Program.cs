97990aac67e69fe8ca430e06c16e507741d6e3f6
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-04-07 11:45:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebeskoTijeloBudicMarin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(2,2);
+            Satelit Mjesec = new Satelit(4,2);
+
+        }
+    }
+}
